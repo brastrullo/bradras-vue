0c02c2d6fc062cb4b7bfc9e86cdb638cc27c4e21
Bradley Rastrullo <brastrullo@gmail.com>
2020-11-20 10:43:08 -0500

    build: add @braid/vue-formulate

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import VueFormulate from '@braid/vue-formulate';
 import('@/assets/index.css');
 
-
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .use(VueFormulate)
+  .mount('#app');
